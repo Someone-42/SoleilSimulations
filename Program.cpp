c4138e52ba3f5890dc0ee4fc515a2aceccd645de
Someone-42 <tristn.lemarchand@gmail.com>
2024-11-14 22:52:32 +0100

    Added simulation views and prototyping

--- Program.cpp
+++ Program.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <memory>
 #include <stdint.h>
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
@@ -12,7 +14,7 @@ uint64_t GetTimems()
 
 class Simulation
 {
-private:
+protected:
   // Time since epoch captured upon call of Start()
   uint64_t startTime;
   // Time in ms since simulation was started
@@ -20,7 +22,8 @@ private:
 
 public:
   PixelGameEngine* engine;
-  Sprite* view;
+  Sprite* view = nullptr;
+  Decal* viewDecal = nullptr;
   vu2d viewPos;
 
   Simulation(PixelGameEngine* engine)
@@ -28,7 +31,14 @@ public:
     this->engine = engine;
   }
 
-  virtual void UpdateView(Sprite* view, vu2d viewPos);
+  void UpdateView(Sprite* view, vu2d viewPos)
+  {
+    delete this->viewDecal;
+    delete this->view;
+    this->view = view;
+    this->viewDecal = new Decal(view);
+    this->viewPos = viewPos;
+  }
 
   virtual void Reset();
 
@@ -46,37 +56,78 @@ public:
   virtual void Render();
 };
 
+class MagnetSimulation : public Simulation
+{
+public:
+  Pixel color;
+
+  MagnetSimulation(PixelGameEngine* engine, Pixel color) : Simulation(engine)
+  {
+    this->color = color;
+  }
+
+  ~MagnetSimulation()
+  {
+    delete this->viewDecal;
+    delete this->view;
+  }
+
+  void Reset() override
+  {
+  }
+
+  void Start() override
+  {
+    Simulation::Start();
+  }
+
+  void Update(float deltaTime) override
+  {
+    Simulation::Update(deltaTime);
+  }
+
+  void Render() override
+  {
+    engine->Clear(color);
+    engine->Draw((timeElapsed) % (engine->ScreenWidth() >> 1), 100 + 10 * sin(timeElapsed), Pixel(255, 0, 0));
+    viewDecal->Update();
+  }
+};
+
 class SimulationWindow : public olc::PixelGameEngine
 {
 private:
-  vector<Simulation*> simulations;
+  vector<unique_ptr<Simulation>> simulations;
 
   Pixel clearColor = Pixel(0, 0, 0);
 
 public:
   SimulationWindow()
   {
+    cout << "ctor\n";
     sAppName = "Simulation";
-    simulations = {};
+    simulations.emplace_back(new MagnetSimulation(this, Pixel(255, 0, 0)));
+    simulations.emplace_back(new MagnetSimulation(this, Pixel(0, 255, 0)));
+    cout << "emplaced \n";
     // TODO: Create default views -> Update simulation view
+    cout << "out ctor\n";
   }
 
   ~SimulationWindow()
   {
-    for (Simulation* simulation : simulations)
-    {
-      delete simulation;
-    }
   }
 
 public:
   bool OnUserCreate() override
   {
-    for (Simulation* simulation : simulations)
+    simulations[0]->UpdateView(new Sprite(ScreenWidth() / 2, ScreenHeight()), vu2d(0, 0));
+    simulations[1]->UpdateView(new Sprite(ScreenWidth() / 2, ScreenHeight()), vu2d(ScreenWidth() / 2, 0));
+    for (auto& simulation : simulations)
     {
       simulation->Reset();
       simulation->Start();
     }
+    cout << "out create\n";
     return true;
   }
 
@@ -85,7 +136,7 @@ public:
     // ---------------------
     //      Update Cycle
     // ---------------------
-    for (Simulation* simulation : simulations)
+    for (auto& simulation : simulations)
     {
       simulation->Update(fElapsedTime);
     }
@@ -93,12 +144,16 @@ public:
     // ---------------------
     //      Render Cycle
     // ---------------------
-    Clear(clearColor);
-    for (Simulation* simulation : simulations)
+    for (auto& simulation : simulations)
     {
+      SetDrawTarget(simulation->view);
       simulation->Render();
-      DrawSprite(simulation->viewPos.x, simulation->viewPos.y, simulation->view);
+      SetDrawTarget(nullptr);
+      DrawDecal(simulation->viewPos, simulation->viewDecal);
     }
+
+    // Going back to main screen for rendering
+
     // TODO: Draw delimiter ? UI ?
     return true;
   }
